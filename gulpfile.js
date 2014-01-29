0cccebef506ac187697fc64d46429aaba9caa877
unknown <bmarcondes@NN-019.nuova2.com>
2014-01-29 10:45:43 -0200

    estruturação

--- gulpfile.js
+++ gulpfile.js
@@ -4,90 +4,81 @@ uglify      = require('gulp-uglify'),
 cssUglify   = require('gulp-minify-css'),
 imagemin    = require('gulp-imagemin'),
 minifyHTML  = require('gulp-minify-html'),
-rename      = require("gulp-rename"),
-concat      = require('gulp-concat'),
+useref      = require('gulp-useref'),
+jsValidate  = require('gulp-jsvalidate'),
 config      = {
   paths: {
-    css    : 'assets/css/*.css',
-    js     : 'assets/js/*js',
-    jsLibs : 'assets/js/libs/*js',
-    imgs   : 'assets/imgs/',
-    html   : 'assets/*.html',
+    css     : 'assets/css/*.css',
+    cssLibs : 'assets/css/libs/*.css',
+    js      : 'assets/js/*.js',
+    jsLibs  : 'assets/js/libs/*.js',
+    imgs    : 'assets/imgs/*',
+    html    : 'assets/html/*.html',
   },
   pathsMin: {
     css    : 'deploy/css/',
     js     : 'deploy/js/',
-    jsLibs : 'assets/js/libs/*js',
     imgs   : 'deploy/imgs/',
     html   : 'deploy/',
   },
   tasks : {
-    js     : 'js',
-    jsLibs : 'jsLibs',
-    css    : 'css',
-    html   : 'html',
-    imgs   : 'imgs'
-  };
+    js       : 'js',
+    jsLibs   : 'jsLibs',
+    css      : 'css',
+    cssLibs  : 'cssLibs',
+    html     : 'html',
+    imgs     : 'imgs'
+  }
 }
 
 gulp.task(config.tasks.js, function() {
   gulp.src(config.paths.js)
+    .pipe(jsValidate())
     .pipe(uglify())
-    .pipe(rename({
-        suffix: '.min'
-    }))
-    .pipe(concat("all.js"))
-    .pipe(gulp.dest(config.pathsMin.js))
+    .pipe(gulp.dest(config.pathsMin.js));
 });
-
 gulp.task(config.tasks.jsLibs, function() {
   gulp.src(config.paths.jsLibs)
     .pipe(uglify())
-    .pipe(concat("plugins.js"))
     .pipe(gulp.dest(config.pathsMin.js))
 });
 
-gulp.task(config.tasks.css, function (e) {
+gulp.task(config.tasks.css, function () {
   gulp.src(config.paths.css)
-    .pipe(rename({
-        suffix: '.min'
-    }))
     .pipe(cssUglify())
     .pipe(gulp.dest(config.pathsMin.css));
+});
+gulp.task(config.tasks.cssLibs, function() {
+  gulp.src(config.paths.cssLibs)
+    .pipe(cssUglify())
+    .pipe(gulp.dest(config.pathsMin.css))
 });	
-
 gulp.task(config.tasks.html, function () {
   gulp.src(config.paths.html)
     .pipe(minifyHTML())
+    .pipe(useref())
     .pipe(gulp.dest(config.pathsMin.html));
 }); 
-
 gulp.task(config.tasks.imgs, function () {
   gulp.src(config.paths.imgs)
     .pipe(imagemin())
     .pipe(gulp.dest(config.pathsMin.imgs));
 });
 
+
 gulp.task('watch', function() {
-  
   gulp.watch(config.paths.html, function() {
     gulp.run(config.tasks.html);
+    gulp.run(config.tasks.imgs);
   });
   gulp.watch(config.paths.css, function() {
     gulp.run(config.tasks.css);
+    gulp.run(config.tasks.cssLibs);
   });
   gulp.watch(config.paths.js, function() {
     gulp.run(config.tasks.js);
-  });
-  gulp.watch(config.paths.jsLibs, function() {
     gulp.run(config.tasks.jsLibs);
   });
-  gulp.watch(config.paths.imgs, function() {
-    gulp.run(config.tasks.imgs);
-  });
-  
-  gutil.log('Is watching!');
-  
 });
 
 gulp.task('default', function() {
